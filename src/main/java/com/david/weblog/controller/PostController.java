4dfb64086f21a6d614d05b32a79537f3af14a238
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-07-28 12:42:53 -0300

    Validação e formatação de texto

--- src/main/java/com/david/weblog/controller/PostController.java
+++ src/main/java/com/david/weblog/controller/PostController.java
@@ -47,6 +47,7 @@ public class PostController {
     @RequestMapping(value = "/newpost", method = RequestMethod.POST)
     public String savePostForm(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
         if(result.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
             return "redirect:/newpost";
         }
         post.setData(LocalDate.now());
